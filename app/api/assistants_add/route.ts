9e4d6831b19834bd10eed3e6659b366032afac98
dafei <dafei@github.com>
2023-11-21 16:24:12 +0800

    1623

--- app/api/assistants_add/route.ts
+++ app/api/assistants_add/route.ts
@@ -9,9 +9,8 @@ export async function POST(req: Request) {
   };
   const payload = await req.json();
 
-    console.log("payload", payload);
-    console.log("req", req);
-  const response = await fetch(url, { method:'POST',headers:headers,body:payload });
+  const body = await JSON.stringify(payload);
+  const response = await fetch(url, { method:'POST',headers:headers,body:body });
   const data = await response.json();
     return new Response(JSON.stringify(data), {status: 200});
   } catch (error) {
